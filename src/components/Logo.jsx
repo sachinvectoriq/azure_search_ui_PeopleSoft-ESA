13baada9e98126cbd593c1fb6259639d1c9b13b6
Solomon Bhaskar <131343533+solomon712@users.noreply.github.com>
2025-06-24 18:50:03 +0530

    Updated Logo back to allegis

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,11 +1,12 @@
 import { Link } from 'react-router-dom';
-import miloLogo from '../assets/milo.png';
+import logoImg from '../assets/group_logo.png';
+
 
 const Logo = () => {
   return (
     <h1 className='text-md flex items-center gap-2 font-semibold text-[#004AAD]'>
       <Link to='/'>
-        <img src={miloLogo} alt='logo' className='w-10' />
+        <img src={logoImg} alt='logo' className='w-36' />
       </Link>
     </h1>
   );
