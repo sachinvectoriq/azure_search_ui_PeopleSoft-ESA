dcddd2bbf15fff8492723f8f8ba4bf172851328f
Solomon Bhaskar <131343533+solomon712@users.noreply.github.com>
2025-08-21 11:28:49 +0530

    Updated the snow ticket

--- src/app/features/chat/chatSlice.js
+++ src/app/features/chat/chatSlice.js
@@ -37,7 +37,7 @@ const getFallbackCitation = () => ({
   id: '0',
   title: 'How to Create a PeopleSoft ESA to Aerotek Support Ticket',
   chunk: 'Please refer to this document for detailed instructions on how to raise a SNOW support ticket when you cannot find the information you are looking for.',
-  parent_id:'https://sthubdevaioc273154123411.blob.core.windows.net/snowticket/How%20to%20Create%20a%20PeopleSoft%20ESA%20to%20Aerotek%20Support%20Ticket%20(1).docx',
+  parent_id:'https://sthubdevaioc273154123411.blob.core.windows.net/snowticket/How%20to%20Create%20a%20PeopleSoft%20Financial%20Support%20Ticket%20.docx',
   isSupportDoc: true
 });
 
